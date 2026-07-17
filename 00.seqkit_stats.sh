ae2ae537f133556d4c23c35e6c16177a1d1d278b
Nokuzotha Nkiwane <139913760+nokuzotha-nkiwane@users.noreply.github.com>
2026-07-17 22:50:25 +0200

    change sample_cli to account for current name convention
    because this is the first script ran in the pipeline it shouel make the sample-specific results directory

--- 00.seqkit_stats.sh
+++ 00.seqkit_stats.sh
@@ -19,10 +19,13 @@ module load app/seqkit/2.7.0
 
 #files and variables
 WORKDIR="${TOMATO_PATH}/SAMPLE_CLI"
-RAW_READS_FQ="${WORKDIR}/raw_reads/SAMPLE_CLI.fastq"
-ALL_RESULTS_DIR="${workdir}/results"
+RAW_READS_FQ="${WORKDIR}/raw_reads/D260405-SAMPLE_CLI_HiFi.fastq.gz"
+ALL_RESULTS_DIR="${WORKDIR}/results"
 SEQKIT_OUT_DIR="RESULTS_DIR"
-OUTPUT_FILE="${SEQKIT_OUT_DIR}/SAMPLE_CLI_seqkit_stats_--all.txt"
+OUTPUT_FILE="${SEQKIT_OUT_DIR}/dSAMPLE_CLI_seqkit_stats_--all.txt"
+
+#make top level results directory
+mkdir -p ${ALL_RESULTS_DIR}
 
 #check read stats of samples
-seqkit stats --all SAMPLE_CLI > ${OUTPUT_FILE}
+seqkit stats --all ${RAW_READS_FQ} > ${OUTPUT_FILE}
