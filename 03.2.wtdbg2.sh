bc69a228feab3e42a24983723c280a912ac73042
Nokuzotha Nkiwane <139913760+nokuzotha-nkiwane@users.noreply.github.com>
2026-07-23 16:31:57 +0200

    initial commit

---
+++ 03.2.wtdbg2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=23:mem=60GB
+#PBS -q bix
+#PBS -l walltime=48:00:00
+#PBS -N SAMPLE_CLI_STEP_PBS
+#PBS -o OUTPUT_FILE_PBS
+#PBS -e ERROR_FILE_PBS
+#PBS -m be
+#PBS -M PBS_EMAIL
+
+#kill execution at first error
+set -euxo pipefail 
+
+#for evaluating variables in ~/.pbsrc
+source ~/.pbsrc
+
+#resource parameters
+THREADS=23
+
+#directories and files
+WORKDIR="${TOMATO_PATH}/SAMPLE_CLI"
+RAW_READS_FQ="${WORKDIR}/raw_reads/D260405-SAMPLE_CLI_HiFi.fastq.gz"
+WTDBG2_DIR="__RESULTS_DIR__"
+TEMP_DIR="${WTDBG2_DIR}/${PBS_JOBID}"
+PREFIX="dSAMPLE_CLI"
+
