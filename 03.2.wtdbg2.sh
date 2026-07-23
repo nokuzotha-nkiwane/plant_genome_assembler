aa1020a28ecc4906f5c9f25c06d4539a06341d0c
Nokuzotha Nkiwane <139913760+nokuzotha-nkiwane@users.noreply.github.com>
2026-07-23 16:38:45 +0200

    make output directories and perform assembly

--- 03.2.wtdbg2.sh
+++ 03.2.wtdbg2.sh
@@ -21,6 +21,18 @@ THREADS=23
 WORKDIR="${TOMATO_PATH}/SAMPLE_CLI"
 RAW_READS_FQ="${WORKDIR}/raw_reads/D260405-SAMPLE_CLI_HiFi.fastq.gz"
 WTDBG2_DIR="__RESULTS_DIR__"
-TEMP_DIR="${WTDBG2_DIR}/${PBS_JOBID}"
+TEMP_DIR="${WTDBG2_DIR}/${PBS_JOBID}_temp"
 PREFIX="dSAMPLE_CLI"
 
+#make temp directory to copy reads to so the original ones are accessible to other scripts
+mkdir -p ${TEMP_DIR}
+cp ${RAW_READS_FQ} "${TEMP_DIR}/"
+RAW_READS_FQ="${TEMP_DIR}/D260405-SAMPLE_CLI_HiFi.fastq.gz"
+
+#move to output directory and perform assembly
+cd ${WTDBG2_DIR}
+wtdbg2 -x ccs -t ${THREADS} -i ${RAW_READS_FQ} -fo ${PREFIX}
+wtpoa-cns -t 16 -i ${PREFIX}.ctg.lay.gz -fo ${PREFIX}.ctg.fa
+
+#delete temp dir
+rm -rf "${TEMP_DIR}"
