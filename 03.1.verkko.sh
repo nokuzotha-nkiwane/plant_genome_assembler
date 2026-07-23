ff715b98318ff88afb185a24a161cb4ad73dde97
Nokuzotha Nkiwane <139913760+nokuzotha-nkiwane@users.noreply.github.com>
2026-07-23 15:01:59 +0200

    initial commit

---
+++ 03.1.verkko.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=23:mem=60GB
+#PBS -q bix
+#PBS -l walltime=48:00:00
+#PBS -N SAMPLE_CLI_STEP_PBS
+#PBS -o OUTPUT_FILE_PBS
+#PBS -e ERROR_FILE_PBS
+#PBS -m be
+#PBS -M PBS_EMAIL
+
+#kill execution at first error
+set -euxo pipefail 
+
+#for evaluating variables in ~/.pbsrc
+source ~/.pbsrc
+
+#resource parameters
+THREADS=23
+
+#load modules
+module load app/miniconda/mamba
+conda activate verkko_2.3..2
+
+#directories and files
+WORKDIR="${TOMATO_PATH}/SAMPLE_CLI"
+RAW_READS_FQ="${WORKDIR}/raw_reads/D260405-SAMPLE_CLI_HiFi.fastq.gz"
+ALL_RESULTS_DIR="${WORKDIR}/results"
+VERKKO_DIR="__RESULTS_DIR__"
+
+#perform assembly
