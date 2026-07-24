2de2cb2983cacc0e925307907b5e5e1bcd74fdd9
Nokuzotha Nkiwane <139913760+nokuzotha-nkiwane@users.noreply.github.com>
2026-07-24 11:13:41 +0200

    run busco after assembly produced

--- 03.2.wtdbg2.sh
+++ 03.2.wtdbg2.sh
@@ -17,22 +17,44 @@ source ~/.pbsrc
 #resource parameters
 THREADS=23
 
+#load modules
+module load app/miniconda/mamba
+conda activate busco_6.1.0
+export _JAVA_OPTIONS="-Xmx8g"
+
 #directories and files
 WORKDIR="${TOMATO_PATH}/SAMPLE_CLI"
 RAW_READS_FQ="${WORKDIR}/raw_reads/D260405-SAMPLE_CLI_HiFi.fastq.gz"
 WTDBG2_DIR="__RESULTS_DIR__"
 TEMP_DIR="${WTDBG2_DIR}/${PBS_JOBID}_temp"
-PREFIX="dSAMPLE_CLI"
+BUSCO_DB_DIR="${TOMATO_PATH}/data"
+BUSCO_DIR="${WTDBG2_DIR}/busco"
+OUTPUT_PREFIX="dSAMPLE_CLI_"
 
 #make temp directory to copy reads to so the original ones are accessible to other scripts
-mkdir -p ${TEMP_DIR}
-cp ${RAW_READS_FQ} "${TEMP_DIR}/"
+mkdir -p "${TEMP_DIR}"
+cp "${RAW_READS_FQ}" "${TEMP_DIR}/"
 RAW_READS_FQ="${TEMP_DIR}/D260405-SAMPLE_CLI_HiFi.fastq.gz"
 
 #move to output directory and perform assembly
-cd ${WTDBG2_DIR}
-wtdbg2 -x ccs -t ${THREADS} -i ${RAW_READS_FQ} -fo ${PREFIX}
-wtpoa-cns -t 16 -i ${PREFIX}.ctg.lay.gz -fo ${PREFIX}.ctg.fa
+cd "${WTDBG2_DIR}"
+wtdbg2 -x ccs -t "${THREADS}" -i "${RAW_READS_FQ}" -fo "${OUTPUT_PREFIX}"
+wtpoa-cns -t 16 -i "${OUTPUT_PREFIX}".ctg.lay.gz -fo "${OUTPUT_PREFIX}".ctg.fa
+
+#check quality of assembled contigs for each haplotype
+#busco
+echo "Running BUSCO for ${OUTPUT_PREFIX}.ctg.fa"
+busco --in "${OUTPUT_PREFIX}".ctg.fa \
+    -m genome \
+    --offline \
+    -l eudicotyledons_odb12 \
+    --download_path "${BUSCO_DB_DIR}" \
+    -c "${THREADS}" \
+    -f \
+    -o "${OUTPUT_PREFIX}" \
+    --out_path "${BUSCO_DIR}"
+
+echo "BUSCO for ${OUTPUT_PREFIX}.ctg.fa complete"
 
 #delete temp dir
 rm -rf "${TEMP_DIR}"
