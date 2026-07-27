1d48adec28a533e70416f389761e927a67042d16
Nokuzotha Nkiwane <139913760+nokuzotha-nkiwane@users.noreply.github.com>
2026-07-27 10:54:23 +0200

    revert script to only perform assemlbly and produce gziped fastas

--- 03.hifiasm.sh
+++ 03.hifiasm.sh
@@ -14,6 +14,10 @@ set -euxo pipefail
 #for evaluating variables in ~/.pbsrc
 source ~/.pbsrc
 
+#load modules
+module load app/miniconda/mamba
+conda activate hifiasm
+
 #resource parameters
 THREADS=23
 
@@ -22,19 +26,14 @@ WORKDIR="${TOMATO_PATH}/SAMPLE_CLI"
 RAW_READS_FQ="${WORKDIR}/raw_reads/D260405-SAMPLE_CLI_HiFi.fastq.gz"
 HIFIASM_DIR="__RESULTS_DIR__"
 HIFIASM_ASM="${HIFIASM_DIR}/dSAMPLE_CLI.asm"
-GFA_FILE="${HIFIASM_DIR}/dSAMPLE_CLI.asm.p_ctg.gfa"
+GFA_P_FILE="${HIFIASM_DIR}/dSAMPLE_CLI.asm.p_ctg.gfa"
+GFA_A_FILE="${HIFIASM_DIR}/dSAMPLE_CLI.asm.a_ctg.gfa"
 TEMP_DIR="${HIFIASM_DIR}/${PBS_JOBID}_temp"
-HIFIASM_OUT_FASTA="${HIFIASM_DIR}/dSAMPLE_CLI_primary.fa"
-BUSCO_DB_DIR="${TOMATO_PATH}/data"
-BUSCO_DIR="${HIFIASM_DIR}/busco"
-OUTPUT_PREFIX="dSAMPLE_CLI_"
-
-#load modules
-module load app/miniconda/mamba
-conda activate hifiasm
+HIFIASM_P_FASTA="${HIFIASM_DIR}/dSAMPLE_CLI_primary.fa"
+HIFIASM_A_FASTA="${HIFIASM_DIR}/dSAMPLE_CLI_alternate.fa"
 
 #make temp directory to copy reads to so the original ones are accessible to other scripts
-mkdir -p "${TEMP_DIR}" "${BUSCO_DIR}"
+mkdir -p "${TEMP_DIR}"
 cp "${RAW_READS_FQ}" "${TEMP_DIR}/"
 RAW_READS_FQ="${TEMP_DIR}/D260405-SAMPLE_CLI_HiFi.fastq.gz"
 
@@ -43,30 +42,18 @@ echo "Performing contig assembly..."
 hifiasm -o "${HIFIASM_ASM}" --primary -t "${THREADS}" -i "${RAW_READS_FQ}" || { echo "Contig assembly failed"; exit 1; }
 echo "Contig assembly complete"
 
+#make an arrary of input gfa files and corresponding output fasta files
+GFA_FILES=("${GFA_P_FILE}" "${GFA_A_FILE}")
+FASTA_FILES=("${HIFIASM_P_FASTA}" "${HIFIASM_A_FASTA}" )
+
 #convert gfa to fasta
-awk '/^S/{print ">"$2; print $3}' "${GFA_FILE}" > "${HIFIASM_OUT_FASTA}"
-gzip -k "${HIFIASM_OUT_FASTA}"
+echo "Converting GFA files to fasta"
+for i in "${!GFA_FILES[@]}"; do
+    awk '/^S/{print ">"$2; print $3}' "${GFA_FILES[$i]}" > "${FASTA_FILES[$i]}" \
+        || { echo "GFA to FASTA conversion failed for ${GFA_FILES[$i]}"; exit 1; }
+done
+gzip -k "${FASTA_FILES[@]}"
 
-#deactivate conda environment
-conda deactivate
-
-#activate busco environment
-conda activate busco_6.1.0
-export _JAVA_OPTIONS="-Xmx8g"
-
-#busco
-echo "Running BUSCO for $( basename "${HIFIASM_OUT_FASTA}" )"
-busco --in "${HIFIASM_OUT_FASTA}" \
-    -m genome \
-    --offline \
-    -l eudicotyledons_odb12 \
-    --download_path "${BUSCO_DB_DIR}" \
-    -c "${THREADS}" \
-    -f \
-    -o "${OUTPUT_PREFIX}" \
-    --out_path "${BUSCO_DIR}"
-
-echo "BUSCO for $( basename "${HIFIASM_OUT_FASTA}" )"
 
 #delete temp dir
 rm -rf "${TEMP_DIR}"
