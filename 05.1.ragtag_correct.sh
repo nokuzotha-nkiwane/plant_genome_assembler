97b061a84340a4d0e41f764448aaadd68ccc4e6d
Nokuzotha Nkiwane <139913760+nokuzotha-nkiwane@users.noreply.github.com>
2026-08-04 19:42:09 +0200

    rename script to show it performs correction

---
+++ 05.1.ragtag_correct.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=23:mem=60GB
+#PBS -q bix
+#PBS -l walltime=15:00:00
+#PBS -N SAMPLE_CLI_STEP_PBS
+#PBS -o OUTPUT_FILE_PBS
+#PBS -e ERROR_FILE_PBS
+#PBS -m be
+#PBS -M PBS_EMAIL
+
+#kill execution at first error
+set -euxo pipefail 
+
+#for evaluating variables in ~/.pbsrc
+source ~/.pbsrc
+
+#load modules
+module load app/miniconda/mamba
+conda activate ragtag
+
+#resource parameters
+THREADS=23
+
+#directories and files
+WORKDIR="${TOMATO_PATH}/SAMPLE_CLI"
+REF_DIR="${TOMATO_PATH}/data/reference_data"
+REF_GENOME="${REF_DIR}/SL5.0.fasta.gz"
+ALL_RESULTS_DIR="${WORKDIR}/results"
+RAGTAG_SCAFFOLD_DIR="__RESULTS_DIR__"
+HIFIASM_DIR="${ALL_RESULTS_DIR}/03.hifiasm"
+P_CONTIGS_IN="${HIFIASM_DIR}/dSAMPLE_CLI_primary_renamed.fa"
+TEMP_DIR="${RAGTAG_SCAFFOLD_DIR}/${PBS_JOBID}_temp"
+
+#make temp directory to fastas to so the original ones are accessible to other scripts
+mkdir -p "${TEMP_DIR}"
+
+#automatically remove TEMP_DIR whenever the script exits (normal or error)
+trap 'rm -rf "${TEMP_DIR}"' EXIT
+
+#copy fastas file to temporary directory
+cp "${P_CONTIGS_IN}" \
+    "${REF_GENOME}" "${TEMP_DIR}/"
+
+#unzip reference fasta
+gzip -d "${TEMP_DIR}/$(basename "${REF_GENOME}")"
+
+#reassign variables to the temp directory versions
+P_CONTIGS_IN="${TEMP_DIR}/$(basename "${P_CONTIGS_IN}")"
+REF_GENOME="${TEMP_DIR}/$(basename "${REF_GENOME}" .gz)"
+
+#scaffold assemblies
+cd "${TEMP_DIR}"
+ragtag.py correct "${REF_GENOME}" "${P_CONTIGS_IN}"
+
+#copy outputs (including directories) to final output directory
+cp -r ragtag_output/* "${RAGTAG_SCAFFOLD_DIR}"
