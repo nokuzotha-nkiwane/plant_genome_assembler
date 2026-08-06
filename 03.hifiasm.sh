24dd84d502ce6d96b4cd78a88cb4d3a848b9961d
Nokuzotha Nkiwane <139913760+nokuzotha-nkiwane@users.noreply.github.com>
2026-08-06 17:59:19 +0200

    run multiple assemblies across dif params

--- 03.hifiasm.sh
+++ 03.hifiasm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=23:mem=60GB
+#PBS -l select=1:ncpus=23:mem=80GB
 #PBS -q bix
 #PBS -l walltime=15:00:00
 #PBS -N SAMPLE_CLI_STEP_PBS
@@ -25,21 +25,25 @@ THREADS=23
 WORKDIR="${TOMATO_PATH}/SAMPLE_CLI"
 RAW_READS_FQ="${WORKDIR}/raw_reads/D260405-SAMPLE_CLI_HiFi.fastq.gz"
 HIFIASM_DIR="__RESULTS_DIR__"
-HIFIASM_ASM="${HIFIASM_DIR}/dSAMPLE_CLI.asm"
+HIFIASM_ASM_l0="${HIFIASM_DIR}/l0/dSAMPLE_CLI.asm"
+HIFIASM_ASM_primary="${HIFIASM_DIR}/primary/dSAMPLE_CLI.asm"
 GFA_P_FILE="${HIFIASM_DIR}/dSAMPLE_CLI.asm.p_ctg.gfa"
 GFA_A_FILE="${HIFIASM_DIR}/dSAMPLE_CLI.asm.a_ctg.gfa"
 TEMP_DIR="${HIFIASM_DIR}/${PBS_JOBID}_temp"
 HIFIASM_P_FASTA="${HIFIASM_DIR}/dSAMPLE_CLI_primary.fa"
 HIFIASM_A_FASTA="${HIFIASM_DIR}/dSAMPLE_CLI_alternate.fa"
+FASTA_PRIMARY_RENAMED="${HIFIASM_DIR}/dSAMPLE_CLI_primary_renamed.fa"
+FASTA_ALTERNATE_RENAMED="${HIFIASM_DIR}/dSAMPLE_CLI_alternate_renamed.fa"
 
 #make temp directory to copy reads to so the original ones are accessible to other scripts
-mkdir -p "${TEMP_DIR}"
+mkdir -p "${TEMP_DIR}" "${HIFIASM_ASM_l0}" "${HIFIASM_ASM_l0}"
 cp "${RAW_READS_FQ}" "${TEMP_DIR}/"
 RAW_READS_FQ="${TEMP_DIR}/D260405-SAMPLE_CLI_HiFi.fastq.gz"
 
 #hifiasm assembly
 echo "Performing contig assembly..."
-hifiasm -o "${HIFIASM_ASM}" --primary -t "${THREADS}" -i "${RAW_READS_FQ}" || { echo "Contig assembly failed"; exit 1; }
+hifiasm -o "${HIFIASM_ASM_primary}" --primary -t "${THREADS}" "${RAW_READS_FQ}" || { echo "Contig assembly failed"; exit 1; }
+hifiasm -o "${HIFIASM_ASM_l0}" -l0 -t "${THREADS}" "${RAW_READS_FQ}" || { echo "Contig assembly failed"; exit 1; }
 echo "Contig assembly complete"
 
 #make an arrary of input gfa files and corresponding output fasta files
@@ -51,6 +55,8 @@ echo "Converting GFA files to fasta"
 for i in "${!GFA_FILES[@]}"; do
     awk '/^S/{print ">"$2; print $3}' "${GFA_FILES[$i]}" > "${FASTA_FILES[$i]}" \
         || { echo "GFA to FASTA conversion failed for ${GFA_FILES[$i]}"; exit 1; }
+
+    sed -E 's/^>(.*)/>dSAMPLE_CLI_\1/' "${FASTA_FILES[$i]}" > "${fasta_outfile_renamed}"
 done
 gzip -k "${FASTA_FILES[@]}"
 
