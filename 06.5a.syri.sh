422e44643fcaa143d06b18d7209eb5055d6ad8a3
Nokuzotha Nkiwane <139913760+nokuzotha-nkiwane@users.noreply.github.com>
2026-08-16 15:45:04 +0200

    initial commit

---
+++ 06.5a.syri.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=23:mem=60GB
+#PBS -q bix
+#PBS -l walltime=15:00:00
+#PBS -N SAMPLE_CLI_STEP_PBS
+#PBS -o OUTPUT_FILE_PBS
+#PBS -e ERROR_FILE_PBS
+#PBS -m be
+#PBS -M PBS_EMAIL
