dcd4097b224377fecd38476f8afeba03f0bb8d41
Nokuzotha Nkiwane <139913760+nokuzotha-nkiwane@users.noreply.github.com>
2026-08-25 13:46:28 +0200

    increase walltime to account for 18 comparisons

--- 06.4.minimap2_ref_scaffold.sh
+++ 06.4.minimap2_ref_scaffold.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l select=1:ncpus=23:mem=60GB
 #PBS -q bix
-#PBS -l walltime=24:00:00
+#PBS -l walltime=72:00:00
 #PBS -N SAMPLE_CLI_STEP_PBS
 #PBS -o OUTPUT_FILE_PBS
 #PBS -e ERROR_FILE_PBS
