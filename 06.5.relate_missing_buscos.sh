61d3cbcd718e637785ffe64bcac660f68e1170ea
Nokuzotha Nkiwane <139913760+nokuzotha-nkiwane@users.noreply.github.com>
2026-08-27 12:22:55 +0200

    add directories and files

--- 06.5.relate_missing_buscos.sh
+++ 06.5.relate_missing_buscos.sh
@@ -16,3 +16,16 @@ set -euxo pipefail
 source ~/.pbsrc
 
 #directories and files
+WORKDIR="${TOMATO_PATH}/SAMPLE_CLI"
+REF_GENOME="${TOMATO_PATH}/data/reference_data/SL5.0.fasta.gz"
+ALL_RESULTS_DIR="${WORKDIR}/results"
+UNPLACED_SCAFFOLDS_FASTA="${ALL_RESULTS_DIR}/05.2.ragtag_scaffold/f15000_d500000/SAMPLE_CLI.f15000_d500000.ragtag.scaffold.unplaced.fasta"
+BUSCO_DIR="${ALL_RESULTS_DIR}/06.1.busco_ragtag/scaffold"
+BUSCO_MISSING_CHRSM_SCAFFOLDS="${BUSCO_DIR}/SAMPLE_CLI.f15000_d500000.ragtag.scaffold.chromosomes_busco/run_solanales_odb10/missing_busco_list.tsv"
+BUSCO_FULL_UNPLACED_SCAFFOLDS="${BUSCO_DIR}/SAMPLE_CLI.f15000_d500000.ragtag.scaffold.unplaced_busco/run_solanales_odb10/full_table.tsv"
+OUTPUT_DIR="__RESULTS_DIR__"
+CMPLTE_FROM_UNPLACED_LIST="${OUTPUT_DIR}/complete_from_full_unplaced.list"
+CONSOLIDATED_MISSINGS_LIST="${OUTPUT_DIR}/consoldated_missings.contigs.list"
+unique_CONSOLIDATED_MISSINGS_LIST="${OUTPUT_DIR}/unique_consoldated_missings.contigs.list"
+unique_CONSOLIDATED_MISSINGS_FASTA="${OUTPUT_DIR}/unique_consoldated_missings.fasta"
+DGENIES_INPUT="${OUTPUT_DIR}/dgenies input"
