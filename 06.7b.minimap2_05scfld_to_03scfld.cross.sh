e69fd6287b2aba939fc3f6436f765935f50168d7
Nokuzotha Nkiwane <139913760+nokuzotha-nkiwane@users.noreply.github.com>
2026-09-02 10:37:37 +0200

    align round 2 scaffolds of samples

--- 06.7b.minimap2_05scfld_to_03scfld.cross.sh
+++ 06.7b.minimap2_05scfld_to_03scfld.cross.sh
@@ -25,10 +25,10 @@ THREADS=23
 WORKDIR_03="${TOMATO_PATH}/03"
 WORKDIR_05="${TOMATO_PATH}/05"
 ASM_ASM_ALN="__RESULTS_DIR__"
-D03_CONTIGS_IN="${WORKDIR_03}/results/03.hifiasm/d03_primary_renamed.fa"
-D05_CONTIGS_IN="${WORKDIR_05}/results/03.hifiasm/d05_primary_renamed.fa"
+D03_CONTIGS_IN="${WORKDIR_03}/results/07.2.ragtag_scaffold_2/f15000_d500000/05.f15000_d500000.ragtag.scaffold.chromosomes"
+D05_CONTIGS_IN="${WORKDIR_05}/results/07.2.ragtag_scaffold_2/f15000_d500000/05.f15000_d500000.ragtag.scaffold.chromosomes"
 TEMP_DIR="${ASM_ASM_ALN}/${PBS_JOBID}_temp"
-ASM_05_03="${ASM_ASM_ALN}/asm_05_03"
+ASM_05_03="${ASM_ASM_ALN}/scaffold_05_03"
 
 #make temp directory to fastas to so the original ones are accessible to other scripts
 mkdir -p "${TEMP_DIR}" "${ASM_05_03}"
@@ -45,5 +45,5 @@ D03_CONTIGS_IN="${TEMP_DIR}/$(basename "${D03_CONTIGS_IN}")"
 D05_CONTIGS_IN="${TEMP_DIR}/$(basename "${D05_CONTIGS_IN}")"
 
 #align assemblies to each other have 05 as reference
-minimap2 -ax asm5 -t "${THREADS}" "${D05_CONTIGS_IN}" "${D03_CONTIGS_IN}" > "${ASM_05_03}/aln5.sam"
+# minimap2 -ax asm5 -t "${THREADS}" "${D05_CONTIGS_IN}" "${D03_CONTIGS_IN}" > "${ASM_05_03}/aln5.sam"
 minimap2 -cx asm5 --cs -t "${THREADS}" "${D05_CONTIGS_IN}" "${D03_CONTIGS_IN}" > "${ASM_05_03}/aln5.paf"
