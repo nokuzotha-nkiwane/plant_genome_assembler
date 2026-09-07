f47f297894a263c01d42288a8414ba968c967abe
Nokuzotha Nkiwane <139913760+nokuzotha-nkiwane@users.noreply.github.com>
2026-09-07 12:23:09 +0200

    make dgenies input directory

--- 07.1.agp_correct.sh
+++ 07.1.agp_correct.sh
@@ -15,8 +15,27 @@ set -euxo pipefail
 source ~/.pbsrc
 
 #load modules
-source /opt/miniconda3/etc/profile.d/conda.sh
+module load app/miniconda/mamba
 conda activate ragtag
 
 #resource allocation
-THREADS=2
+THREADS=23
+
+
+# directories and files
+WORKDIR="${TOMATO_PATH}/SAMPLE_CLI"
+REF_DIR="${TOMATO_PATH}/data/reference_data"
+REF_GENOME="${REF_DIR}/SL5.0.unplaced_removed.fasta.gz"
+ALL_RESULTS_DIR="${WORKDIR}/results"
+RAGTAG_SCAFFOLD_DIR="__RESULTS_DIR__"
+INPUT_FASTA="${ALL_RESULTS_DIR}/05.1.ragtag_correct/ragtag.correct.fasta"
+AGP="${ALL_RESULTS_DIR}/06.5.relate_missing_buscos/ragtag.scaffold_to_correct.agp"
+AGP2FASTA_DIR="${RAGTAG_SCAFFOLD_DIR}/agp2fasta"
+OUTPUT_FASTA="${AGP2FASTA_DIR}/corrected.fasta"
+OUTPUT_FASTA_RENAMED="${AGP2FASTA_DIR}/dSAMPLE_CLI.renamed_corrected.fasta"
+RAGTAG_OUTPUT_DIR="${RAGTAG_SCAFFOLD_DIR}/ragtag_output"
+MINIMAP_PAF="${RAGTAG_OUTPUT_DIR}/dSAMPLE_CLI_to_ref_aln5.paf"
+DGENIES_INPUT="${RAGTAG_SCAFFOLD_DIR}/dgenies_input"
+
+#make dgenies input directory
+mkdir -p "${DGENIES_INPUT}"
