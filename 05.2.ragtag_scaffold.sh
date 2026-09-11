87b8eb9c6d2afcfa3c10ff6ac07388e00622f403
Nokuzotha Nkiwane <139913760+nokuzotha-nkiwane@users.noreply.github.com>
2026-09-11 22:15:41 +0200

    scaffold run for --primary assembly

--- 05.2.ragtag_scaffold.sh
+++ 05.2.ragtag_scaffold.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=4:mem=40GB
+#PBS -l select=1:ncpus=4:mem=4GB
 #PBS -q bix
-#PBS -l walltime=8:00:00
+#PBS -l walltime=1:00:00
 #PBS -N SAMPLE_CLI_STEP_PBS
 #PBS -o OUTPUT_FILE_PBS
 #PBS -e ERROR_FILE_PBS
@@ -26,9 +26,10 @@ WORKDIR="${TOMATO_PATH}/SAMPLE_CLI"
 REF_DIR="${TOMATO_PATH}/data/reference_data"
 REF_GENOME="${REF_DIR}/SL5.0.unplaced_removed.fasta.gz"
 ALL_RESULTS_DIR="${WORKDIR}/results"
-RAGTAG_SCAFFOLD_DIR="__RESULTS_DIR__"
-P_CONTIGS_IN="${ALL_RESULTS_DIR}/05.1.ragtag_correct/ragtag.correct.fasta"
+# RAGTAG_SCAFFOLD_DIR="__RESULTS_DIR__"
+P_CONTIGS_IN="${ALL_RESULTS_DIR}/SL5.0_analyses_primary/05.1.ragtag_correct/ragtag.correct.fasta"
 TEMP_DIR="${RAGTAG_SCAFFOLD_DIR}/${PBS_JOBID}_temp"
+RAGTAG_SCAFFOLD_DIR="${ALL_RESULTS_DIR}/SL5.0_analyses_primary/05.2.ragtag_scaffold"
 
 #make temp directory to fastas to so the original ones are accessible to other scripts
 mkdir -p "${TEMP_DIR}"
