6c38480f83f9a338a38d8d718a6ab225e776dd92
Nokuzotha Nkiwane <139913760+nokuzotha-nkiwane@users.noreply.github.com>
2026-09-11 22:21:10 +0200

    write to original output dir

--- 05.2.ragtag_scaffold.sh
+++ 05.2.ragtag_scaffold.sh
@@ -26,10 +26,9 @@ WORKDIR="${TOMATO_PATH}/SAMPLE_CLI"
 REF_DIR="${TOMATO_PATH}/data/reference_data"
 REF_GENOME="${REF_DIR}/SL5.0.unplaced_removed.fasta.gz"
 ALL_RESULTS_DIR="${WORKDIR}/results"
-# RAGTAG_SCAFFOLD_DIR="__RESULTS_DIR__"
+RAGTAG_SCAFFOLD_DIR="__RESULTS_DIR__"
 P_CONTIGS_IN="${ALL_RESULTS_DIR}/SL5.0_analyses_primary/05.1.ragtag_correct/ragtag.correct.fasta"
 TEMP_DIR="${RAGTAG_SCAFFOLD_DIR}/${PBS_JOBID}_temp"
-RAGTAG_SCAFFOLD_DIR="${ALL_RESULTS_DIR}/SL5.0_analyses_primary/05.2.ragtag_scaffold"
 
 #make temp directory to fastas to so the original ones are accessible to other scripts
 mkdir -p "${TEMP_DIR}"
