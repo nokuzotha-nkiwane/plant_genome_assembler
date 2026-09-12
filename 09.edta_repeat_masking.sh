339cfa505bafb35fe3a4447de6614d840c3417c2
Nokuzotha Nkiwane <139913760+nokuzotha-nkiwane@users.noreply.github.com>
2026-09-12 09:31:22 +0200

    change naming for 3

--- 09.edta_repeat_masking.sh
+++ 09.edta_repeat_masking.sh
@@ -27,7 +27,7 @@ ALL_RESULTS_DIR="${WORKDIR}/results"
 OUTPUT_DIR="__RESULTS_DIR__"
 REF_DIR="${TOMATO_PATH}/data/reference_data"
 CDS="${REF_DIR}/SL5.cds.fa.gz"
-RAGATAG_SCAFFOLD_FASTA="${ALL_RESULTS_DIR}/SL5.0_analyses_l0/07.1.agp_correct/ragtag_output/dSAMPLE_CLI.ragtag.scaffold.chromosomes.fasta"
+RAGATAG_SCAFFOLD_FASTA="${ALL_RESULTS_DIR}/07.1.agp_correct/ragtag_output/dSAMPLE_CLI.ragtag.scaffold.chromosomes.fasta"
 EDTA_IMAGE="/new-home/25086138/my_environments/edta/EDTA.sif"
 TOOL_ENV="/usr/local/bin/"
 
