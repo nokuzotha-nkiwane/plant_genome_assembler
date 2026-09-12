378e5b413e42200bf6a140bc9eb40bd462272118
Nokuzotha Nkiwane <139913760+nokuzotha-nkiwane@users.noreply.github.com>
2026-09-12 15:00:10 +0200

    increase resources and update output dir

--- 09.edta_repeat_masking.sh
+++ 09.edta_repeat_masking.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#PBS -l ncpus=64
-#PBS -l mem=80GB
+#PBS -l ncpus=80
+#PBS -l mem=100GB
 #PBS -q bix
 #PBS -l walltime=96:00:00
 #PBS -o OUTPUT_FILE_PBS
@@ -21,14 +21,25 @@ module load app/EDTA2/2.2.2
 THREADS=64
 
 #directories and files
-TOMATO_PATH="/new-home/25086138/masters/tomato"
 WORKDIR="${TOMATO_PATH}/SAMPLE_CLI"
 ALL_RESULTS_DIR="${WORKDIR}/results"
-OUTPUT_DIR="${ALL_RESULTS_DIR}/09.edta_repeat_masking"
+OUTPUT_DIR="__RESULTS_DIR__"
 REF_DIR="${TOMATO_PATH}/data/reference_data"
 CDS="${REF_DIR}/SL5.cds.fa.gz"
+logfile="${OUTPUT_DIR}/edta.log"
 RAGATAG_SCAFFOLD_FASTA="${ALL_RESULTS_DIR}/07.1.agp_correct/ragtag_output/dSAMPLE_CLI.ragtag.scaffold.chromosomes.fasta"
 # EDTA_IMAGE="/new-home/25086138/my_environments/edta/EDTA.sif"
 # TOOL_ENV="/usr/local/bin/"
 
-EDTA.pl --genome "${RAGATAG_SCAFFOLD_FASTA}" --sensitive 1 --anno 1 -t 64
+cd "${OUTPUT_DIR}"
+EDTA.pl --genome "${RAGATAG_SCAFFOLD_FASTA}" --sensitive 1 --anno 1 -t 80 > >(tee -a "${logfile}") 2>&1 &
+edta_pid=$!
+
+while kill -0 "${edta_pid}" 2>/dev/null; do
+    if grep -qE "^ERROR|FATAL|die at" "${logfile}"; then
+        kill -TERM "${edta_pid}"
+        wait "${edta_pid}" 2>/dev/null
+        return 1
+    fi
+    sleep 10
+done
